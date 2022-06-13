a906355eb71fdb5f3dd1bb15b0f85b80b6540088
Andrey <andrey799skvorzov@gmail.com>
2022-06-13 16:27:36 +0300

    all expect system

---
+++ WindowsFormsApp1/WindowsFormsApp1/Answers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Answers
+    {
+        public static double bigAsb; /* Расчет велечины армирования плитной части фундамента по ширине, соответствующей предельному изгибающему моменту и продавливающей оси */
+        public static double bigPult; /* РАсчет несущей способности прямоугольного железообразного фундамента */
+    }
+}
--- WindowsFormsApp1/WindowsFormsApp1/IntermediateValuesOfFormulas.cs
+++ WindowsFormsApp1/WindowsFormsApp1/IntermediateValuesOfFormulas.cs
@@ -8,11 +8,45 @@ namespace WindowsFormsApp1
 {
     public class IntermediateValuesOfFormulas
     {
-        public static double lambda;
-        public static double mi0;
-        public static double mi1;
-        public static double mi2;
-        public static double bigE;
-        public static double bigI;
+        public static double lambda; /* Расчет коэффициента несущей способности */
+        public static double mi0; /* Расчет коэффициента несущей способности */
+        public static double mi1; /* Расчет коэффициента несущей способности */
+        public static double mi2; /* Расчет коэффициента несущей способности */
+        public static double bigE; /* Расчет коэффициента несущей способности */
+        public static double bigI; /* Расчет коэффициента несущей способности */
+        public static double a; /* Расчет коэффициента несущей способности */
+        public static double bigF; /* Расчет коэффициента несущей способности */
+        public static double bigR; /* Расчет коэффициента несущей способности */
+        public static double bigNy; /* Расчет коэффициента несущей способности */
+        public static double bigNq; /* Расчет коэффициента несущей способности */
+        public static double bigNc; /* Расчет коэффициента несущей способности */
+        public static double bigA; /* Расчет коэффициента для условия прочности Кулона-Мора */
+        public static double bigC; /* Расчет коэффициента для условия прочности Кулона-Мора */
+        public static double bigP; /* Расчет предельной нагрузки под подошвой фундамента */
+        public static double bigNp; /* Расчет коэффициента для условия прочности Кулона-Мора */
+        public static double formulh0; /* Расчет рабочей высоты плитной части фундамента */
+        public static double forceF; /* Расчет предельной продавливающей силы по ширине фундамента */
+        public static double bigN; /* Расчет предельной продавливающей силы по ширине фундамента */
+        public static double bx; /* Расчет расстояния от жесткой части фундамента до неподвижной точки в плитной части по ширине */
+        public static double bigMultb; /* Расчет предельного изгибающего момента по ширине плиитной части фундамента */
+        public static double bigNHard; /* Расчет предельной продавливающей силы по длине фундамента */
+        public static double bigLxHard; /* Расчет расстояния от жесткой части фундамента до неподвижной точки в плитной части по длине */
+        public static double bigMultbHard; /* Расчет предельного изгибающего момента по длине плитной части фундамента */
+        public static double bigAsLHard; /* Расчет величины армирования плитной части фундамента по длине, соответствующей предельному изгибающему моменту и продавливающей силе */
+        public static double bigMb; /* Расчет предельного изгибающего момента по ширине плитной части фундамента */
+        public static double bigMl; /* Расчет предельного изгибающего момента по длине плитной части фундамента */
+        public static double bigMub; /* Рачет безразмерных параметров для системы уравнений */
+        public static double bigMul; /* Рачет безразмерных параметров для системы уравнений */
+        public static double infAlpha; /* Рачет безразмерных параметров для системы уравнений */
+        public static double infBi; /* Рачет безразмерных параметров для системы уравнений */
+        public static double ita; /* Рачет безразмерных параметров для системы уравнений */
+        public static double epselentB; /* Значения системы уравнений */
+        public static double epselentL; /* Значения системы уравнений */
+        public static double widthB1; /* Расчет приведенной ширины подошвы фундамента */
+        public static double lengthL1; /* Расчет приведенной длины подошвы фундамента */
+        public static double ita1; /* Расчет коэффициентов формы подошвы фундамента */
+        public static double epselentY; /* Расчет коэффициентов формы подошвы фундамента */
+        public static double epselentQ; /* Расчет коэффициентов формы подошвы фундамента */
+        public static double epselentC; /* Расчет коэффициентов формы подошвы фундамента */
     }
 }
